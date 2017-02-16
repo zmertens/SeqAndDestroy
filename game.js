cc7fc037626ab9821f46e03a9427593b446c1d87
Anders Pitman <tapitman11@gmail.com>
2017-02-15 18:29:50 -0700

    Prepare for sprites

--- game.js
+++ game.js
@@ -1,24 +1,14 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', 
-	{ create: create, render: render });
+	{ preload: preload, create: create, update: update, render: render });
 
-var circle = [];
-var floor;
-var offsetX = 70; // roughly the diameter
-var offsetY = 70;
+function preload() {
+}
 
 function create() {
-	for (var i = 0; i != 25; ++i) {
-		for (var j = 0; j != 10; ++j) {
-			circle = new Phaser.Circle(/*game.world.centerX*/i * offsetX, j * offsetY, 64)
-		}
+}
 
-	}
+function update() {
 }
 
 function render() {
-	for (var i = 0; i != 250; ++i) {
-		game.debug.geom(circle[i], '#cfffff');
-	}
-	// game.debug.text('Diameter: ' + circle.diameter, 50, 200);
-	// game.debug.text('Circumference : ' + circle.circumference(), 50, 250);
 }
