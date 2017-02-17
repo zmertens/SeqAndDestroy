4e71584d28a97000813edfeaac5f0c01e97eb12b
Anders Pitman <tapitman11@gmail.com>
2017-02-16 21:17:14 -0700

    Added some physics to mockup

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', 
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', 
 	{ preload: preload, create: create, update: update, render: render });
 
 
-var objects = {};
+var obj = {};
+var fireRate = 100;
+var nextFire;
 
 
 function preload() {
@@ -11,25 +13,52 @@ function preload() {
 }
 
 function create() {
-
   game.stage.backgroundColor = "#333333";
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  //game.physics.arcade.gravity.y = 100;
+  nextFire = game.time.now + fireRate;
+
   var nucFac = nucleotides.createNucleotideFactory({ game: game });
 
   var rowsCount = 5;
   var colsCount = 20;
 
+  obj.nucleotides = game.add.group();
   for (var i = 0; i < rowsCount; i++) {
     for (var j = 0; j < colsCount; j++) {
-      nucFac.createRandomNucleotide({ x: j*40 + 20, y: i*40 + 20 });
+      var nuc = nucFac.createRandomNucleotide({ x: j*40 + 20, y: i*40 + 20 });
+      obj.nucleotides.add(nuc);
     }
   }
 
+  obj.rna = game.add.group();
   for (var i = 0; i < 6; i++) {
-    nucFac.createRandomNucleotide({ x: i*40 + 300, y: 580 });
+    var nuc = nucFac.createRandomNucleotide({ x: i*40 + 300, y: 580 });
+    obj.rna.add(nuc);
   }
+
+  game.physics.enable(obj.nucleotides, Phaser.Physics.ARCADE);
+  obj.nucleotides.setAll('body.immovable', true);
+
+  game.physics.enable(obj.rna, Phaser.Physics.ARCADE);
 }
 
 function update() {
+  if (game.time.now > nextFire && game.input.activePointer.isDown) {
+    nextFire = game.time.now + fireRate;
+
+    for (var i = 0; i < obj.rna.length; i++) {
+      var x = game.input.x + i*40;
+      game.physics.arcade.moveToXY(obj.rna.getChildAt(i), x, game.input.y, 500);
+    }
+  }
+
+  game.physics.arcade.collide(obj.nucleotides, obj.rna, collisionHandler, null,
+    this);
+}
+
+function collisionHandler(obj1, obj2) {
+  obj2.kill();
 }
 
 function render() {
