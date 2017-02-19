20d8a564929174cf7d78ae7f6cc0543d4cbe0a6e
Anders Pitman <tapitman11@gmail.com>
2017-02-18 18:02:34 -0700

    Implemented snap-to-grid

--- game.js
+++ game.js
@@ -1,15 +1,20 @@
 (function() {
   "use strict";
 
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', 
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'SeqAndDestroy', 
 	  { preload: preload, create: create, update: update, render: render });
 
+  var width = 38.4;
+  var height = 38.4;
 
   var nucFac;
-  var obj = {};
   var fireRate = 100;
   var nextFire;
   var matched = false;
+  var siRNAsMoving = false;
+  var siRNAs;
+  var rna;
+  var snapped = false;
 
 
   function preload() {
@@ -28,52 +33,59 @@
     var rowsCount = 5;
     var colsCount = 20;
 
-    obj.nucleobases = game.add.group();
-    obj.nucleobases.inputEnableChildren = true;
-    obj.nucleobases.onChildInputDown.add(nucOnDown, this);
+    rna = game.add.group();
+    rna.inputEnableChildren = true;
+    rna.onChildInputDown.add(nucOnDown, this);
     for (var i = 0; i < rowsCount; i++) {
       for (var j = 0; j < colsCount; j++) {
-        var nuc = nucFac.createRandomNucleobase({ x: j*40 + 20, y: i*40 + 20 });
-        obj.nucleobases.add(nuc);
+        var nuc = nucFac.createRandomNucleobase(
+          { x: j*width + width/2, y: i*height + height/2 });
+        rna.add(nuc);
       }
     }
 
-    obj.miRNA = game.add.group();
-    obj.miRNA.inputEnableChildren = true;
-    obj.miRNA.onChildInputDown.add(miRNAOnDown, this);
-    obj.miRNA.enableBody = true;
-    for (var i = 0; i < 6; i++) {
-      var nuc = nucFac.createRandomNucleobase({ x: i*40 + 300, y: 580 });
-      obj.miRNA.add(nuc);
+    siRNAs = game.add.group();
+    siRNAs.inputEnableChildren = true;
+    siRNAs.onChildInputDown.add(siRNAsOnDown, this);
+    siRNAs.enableBody = true;
+    for (var i = 0; i < 3; i++) {
+      var nuc = nucFac.createRandomNucleobase({ x: i*width + 350, y: 580 });
+      siRNAs.add(nuc);
     }
 
-    game.physics.enable(obj.nucleobases, Phaser.Physics.ARCADE);
-    obj.nucleobases.setAll('body.immovable', true);
+    game.physics.enable(rna, Phaser.Physics.ARCADE);
+    rna.setAll('body.immovable', true);
 
-    game.physics.enable(obj.miRNA, Phaser.Physics.ARCADE);
+    game.physics.enable(siRNAs, Phaser.Physics.ARCADE);
   }
 
   function update() {
-    if (game.time.now > nextFire && game.input.activePointer.isDown) {
-      var allChosen = true;
+    if (!siRNAsMoving) {
 
-      obj.miRNA.forEach(function(rna) {
-        if (rna.data.nucleobaseType === 'placeholder') {
-          allChosen = false;
-        }
-      });
+      if (game.time.now > nextFire && game.input.activePointer.isDown) {
+        var allChosen = true;
 
-      if (allChosen) {
-        for (var i = 0; i < obj.miRNA.length; i++) {
-          var x = game.input.x + i*40;
-          game.physics.arcade.moveToXY(obj.miRNA.getAt(i), x, game.input.y, 500);
-          //game.add.tween(obj.miRNA.getAt(i)).to({ x: x, y: game.input.y }, 2000).start();
+        siRNAs.forEach(function(rna) {
+          if (rna.data.nucleobaseType === 'placeholder') {
+            allChosen = false;
+          }
+        });
+
+        if (allChosen) {
+
+          siRNAsMoving = true;
+
+          for (var i = 0; i < siRNAs.length; i++) {
+            var x = game.input.x + i*width;
+            game.physics.arcade.moveToXY(siRNAs.getAt(i), x, game.input.y,
+              500);
+          }
         }
+
       }
-
     }
 
-    game.physics.arcade.overlap(obj.nucleobases, obj.miRNA, overlapHandler,
+    game.physics.arcade.overlap(rna, siRNAs, overlapHandler,
       null, this);
 
     if (!matched) {
@@ -84,47 +96,53 @@
   function render() {
   }
 
-  function overlapHandler(obj1, obj2) {
-    if (matchedBase(obj1, obj2)) {
-      //obj2.body.velocity.x = 0;
-      //obj2.body.velocity.y = 0;
-      obj2.data.matched = true;
-      obj2.data.complementX = obj1.body.x;
-      obj2.data.complementY = obj1.body.y;
+  function overlapHandler(rna, siRNA) {
+
+    if (!snapped) {
+      snapToGrid();
+      snapped = true;
+    }
+
+    siRNA.data.overlapping = true;
+
+    if (matchedBase(rna, siRNA)) {
+      siRNA.data.matched = true;
     }
   }
 
   function nucOnDown(sprite) {
   }
 
-  // Cycles through the available nucleobases
-  function miRNAOnDown(sprite) {
-    nextFire = game.time.now + fireRate;
+  // Cycles through the available rna
+  function siRNAsOnDown(sprite) {
+    if (!siRNAsMoving) {
+      nextFire = game.time.now + fireRate;
 
-    // Once you've switched away from the placeholder, there's no way to get back
-    // to it.
-    if (sprite.data.nucleobaseType === 'placeholder' ||
-        sprite.data.nucleobaseType === 'uracil') {
-      replacePlaceholder(sprite, nucFac.createAdenine.bind(nucFac));
-    }
-    else if (sprite.data.nucleobaseType === 'adenine') {
-      replacePlaceholder(sprite, nucFac.createCytosine.bind(nucFac));
-    }
-    else if (sprite.data.nucleobaseType === 'cytosine') {
-      replacePlaceholder(sprite, nucFac.createGuanine.bind(nucFac));
-    }
-    else if (sprite.data.nucleobaseType === 'guanine') {
-      replacePlaceholder(sprite, nucFac.createUracil.bind(nucFac));
-    }
-    else {
-      throw "Invalid nucleobaseType";
+      // Once you've switched away from the placeholder, there's no way to get
+      // back to it.
+      if (sprite.data.nucleobaseType === 'placeholder' ||
+          sprite.data.nucleobaseType === 'uracil') {
+        replacePlaceholder(sprite, nucFac.createAdenine.bind(nucFac));
+      }
+      else if (sprite.data.nucleobaseType === 'adenine') {
+        replacePlaceholder(sprite, nucFac.createCytosine.bind(nucFac));
+      }
+      else if (sprite.data.nucleobaseType === 'cytosine') {
+        replacePlaceholder(sprite, nucFac.createGuanine.bind(nucFac));
+      }
+      else if (sprite.data.nucleobaseType === 'guanine') {
+        replacePlaceholder(sprite, nucFac.createUracil.bind(nucFac));
+      }
+      else {
+        throw "Invalid nucleobaseType";
+      }
     }
   };
 
   function replacePlaceholder(sprite, constructor) {
     var newNuc = constructor({ x: sprite.x, y: sprite.y });
-    var index = obj.miRNA.getIndex(sprite);
-    obj.miRNA.addAt(newNuc, index);
+    var index = siRNAs.getIndex(sprite);
+    siRNAs.addAt(newNuc, index);
     sprite.destroy();
   }
 
@@ -144,26 +162,76 @@
   }
 
   function checkMatches() {
-    var allMatched = true;
+    var allOverlapped = true;
 
-    for (var i = 0; i < obj.miRNA.length; i++) {
-      var rna = obj.miRNA.getAt(i);
+    for (var i = 0; i < siRNAs.length; i++ ) {
+      var rna = siRNAs.getAt(i);
 
-      if (!rna.data.matched) {
-        allMatched = false;
+      if (!rna.data.overlapping) {
+        allOverlapped = false;
         break;
       }
     }
 
-    if (allMatched) {
-      matched = true;
-      //obj.miRNA.setAll('body.velocity.x', 0);
-      //obj.miRNA.setAll('body.velocity.y', 0);
-      obj.miRNA.forEach(function(rna) {
-        rna.body.moves = false;
-        game.add.tween(rna).to({ x: rna.data.complementX, y: rna.data.complementY }, 1000).start();
-      });
+    if (allOverlapped) {
+      var allMatched = true;
+
+      for (var i = 0; i < siRNAs.length; i++) {
+        var rna = siRNAs.getAt(i);
+
+        if (!rna.data.matched) {
+          allMatched = false;
+          break;
+        }
+      }
+
+      if (allMatched) {
+        matched = true;
+      }
+      else {
+        siRNAs.destroy();
+      }
     }
+  }
+
+  function snapToGrid() {
+
+    siRNAs.forEach(function(siRNA) {
+      var gridCorrection = computeGridCorrection(siRNA);
+
+      siRNA.body.velocity.x = 0;
+      siRNA.body.velocity.y = 0;
+      siRNA.x = siRNA.x + gridCorrection.x;
+      siRNA.y = siRNA.y + gridCorrection.y - height/2;
+    });
+  }
+
+  function computeGridCorrection(siRNA) {
+    var xCorrectionPixels = computeAxisCorrection(siRNA.x, width);
+    var yCorrectionPixels = computeAxisCorrection(siRNA.y, height);
+
+    return { x: xCorrectionPixels, y: yCorrectionPixels };
+  }
+
+  function computeAxisCorrection(value, divisionSize) {
+    var globalOffset = divisionSize / 2;
+    var axisRatio = (value + globalOffset) / divisionSize;
+    var divisionsCount = Math.floor(axisRatio);
+    var axisOffset = axisRatio - divisionsCount;
+
+    var snapNegative = axisOffset < 0.5;
+
+    var axisCorrection;
+    if (snapNegative) {
+      axisCorrection = -axisOffset;
+    }
+    else {
+      axisCorrection = 1 - axisOffset;
+    }
+
+    var axisCorrectionPixels = divisionSize * axisCorrection;
+
+    return axisCorrectionPixels;
   }
 
 })();
