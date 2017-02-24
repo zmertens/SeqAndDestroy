e80af9ad5c4efd9fb3ebd8275b63192fc7abba0c
zach <zmertens@asu.edu>
2017-02-24 11:47:06 -0700

    make shader a str instead of an asset

--- game.js
+++ game.js
@@ -1,6 +1,50 @@
 (function() {
   "use strict";
 
+  var bacteriaShaderStr = `
+    /*
+    shader from: http://phaser.io/examples/v2/filters/multiple-shaders
+    */
+
+    precision mediump float;
+
+    uniform float     time;
+    uniform vec2      resolution;
+    uniform vec2      mouse;
+
+    float length2(vec2 p) { return dot(p, p); }
+
+    float noise(vec2 p){
+        return fract(sin(fract(sin(p.x) * (43.13311)) + p.y) * 31.0011);
+    }
+
+    float worley(vec2 p) {
+        float d = 1e30;
+        for (int xo = -1; xo <= 1; ++xo) {
+            for (int yo = -1; yo <= 1; ++yo) {
+                vec2 tp = floor(p) + vec2(xo, yo);
+                d = min(d, length2(p - tp - vec2(noise(tp))));
+            }
+        }
+        return 3.0*exp(-4.0*abs(2.0*d - 1.0));
+    }
+
+    float fworley(vec2 p) {
+        return sqrt(sqrt(sqrt(
+        1.1 * // light
+        worley(p*5. + .3 + time*.0525) *
+        sqrt(worley(p * 50. + 0.3 + time * -0.15)) *
+        sqrt(sqrt(worley(p * -10. + 9.3))))));
+    }
+
+    void main() {
+        vec2 uv = gl_FragCoord.xy / resolution.xy;
+        float t = fworley(uv * resolution.xy / 1500.0);
+        t *= exp(-length2(abs(0.7*uv - 1.0)));
+        gl_FragColor = vec4(t * vec3(0.1, 1.5*t, 1.2*t + pow(t, 0.5-t)), 1.0);
+    }
+  `;
+
   var gameWidth = 800;
   var gameHeight = 600;
 
@@ -31,6 +75,8 @@
     game.load.image('cytosine', 'assets/cytosine.png');
     game.load.image('guanine', 'assets/guanine.png');
     game.load.image('uracil', 'assets/uracil.png');
+    game.load.image('ball', 'assets/ball_grayscale.png');
+    game.load.image('square', 'assets/block_solid.png');
     game.load.shader('bacteria', 'assets/bacteria.frag');
   }
 
@@ -38,8 +84,7 @@
     game.stage.backgroundColor = "#333333";
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    bacteriaFilter = new Phaser.Filter(game, null,
-      game.cache.getShader('bacteria'));
+    bacteriaFilter = new Phaser.Filter(game, null, bacteriaShaderStr);
     bacteriaFilter.setResolution(gameWidth, gameHeight);
     bacteriaSprite = game.add.sprite();
     bacteriaSprite.width = gameWidth;
