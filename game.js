a54449b47cad6bd0d6d5e771210a50767565d5c5
zmertens <zmertens@asu.edu>
2018-02-19 18:30:15 -0700

    Got DAWG hooked into SnD \ Basic functionality tested, API is still flakey and weakly defined

--- game.js
+++ game.js
@@ -9,4 +9,22 @@
   game.state.add('playState', playState);
   game.state.start('preloadState');
 
+  // Test DAWG functionality
+  var low = 0;
+  var high = 10000;
+  console.log(Module.getRandomInt(low,high));
+  console.log(Module.getRandomInt(low,high));
+  console.log(Module.getRandomInt(low,high));
+  console.log(Module.getRandomInt(low,high));
+  console.log(Module.getRandomFloat(low,high));
+  console.log(Module.getRandomFloat(low,high));
+  console.log(Module.getRandomFloat(low,high));
+
+  var dawgWrapper = new Module.DawgWrapper(4);
+  console.log("getRandom(0, 10000)", dawgWrapper.getRandom(0, 10000));
+  console.log("getRandom(0, 10000)", dawgWrapper.getRandom(0, 10000));
+  console.log("getRandom(0, 10000)", dawgWrapper.getRandom(0, 10000));
+  console.log("getRandom(0, 10000)", dawgWrapper.getRandom(0, 10000));
+  dawgWrapper.delete();
+
 })();
